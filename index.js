473813e52dc885e5d1cbce3465081de0c3b186bf
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-12-17 08:57:19 -0500

    Update index.js
    add comments, guides, and refactored ranking logic from for loop dependent to while loop dependent

--- index.js
+++ index.js
@@ -1,38 +1,69 @@
 const puppeteer = require("puppeteer");
 require('dotenv').config();
 
-var url4 = "https://webapps2.uc.edu/elce/Student/Position";
 
-let username = process.env.UC_USERNAME;
-let password = process.env.UC_PASSWORD;
+// TODO - supply your input here
+const username = process.env.UC_USERNAME; // UC username 
+const password = process.env.UC_PASSWORD; // UC password
+const positionsToRank = 10; // The number of positions to rank in one go
+// The ELCE PAL url where the script can start ranking
+const url = "https://webapps2.uc.edu/elce/Student/Position";
 
-async function scrape() {
+
+async function rank() {
+  // Launch browser and go to desired page
   const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(url4);
+  const page = (await browser.pages())[0];
+  await page.goto(url);
 
-  await page.screenshot({ path: "elce_pal_0.png" });
-
+  // Enter username and password
   await page.waitForSelector("#password");
   await page.waitForSelector("#username");
-
   await page.type("#username", username);
   await page.type("#password", password);
+  // Click login
   await page.click(".btn.btn-block.btn-lg");
 
-  await page.waitForSelector("td .btn.btn-danger.btn-md", { timeout: 100000 }).then(async () => {
-    for (let i = 0; i < 20; i++) {
-      await page.click("td .btn.btn-danger.btn-md"); // click rank
-      await page.waitForSelector("#saveButton"); // wait for page load
-      await page.click("#PositionRankId_2"); // very interested
-      await page.click("#saveButton"); // save button
-      await page.waitForSelector("td .btn.btn-danger.btn-md");
-    }
-    await page.screenshot({ path: "elce_pal.png", fullPage: true });
-    await browser.close();
-  });
+  // ...
+  // This is where you check your DUO Mobile app to accept push notification
+  // Once that is done, the code below will run
+  // ...
 
+
+  // Query selectors for page  
+  const rankButtonSelector = "td .btn.btn-danger.btn-md"
+  const saveButtonSelector = "#saveButton"
+  const veryInterestedSelector = "#PositionRankId_2"
+
+  // Make sure rank button is present on page and set conditions
+  await page.waitForSelector(rankButtonSelector);
+  let hasRankButton = page.$(rankButtonSelector);
+  let ranked = 0;
+
+  // Start ranking
+  while (hasRankButton && ranked < positionsToRank) {
+
+    await page.click(rankButtonSelector); // click rank
+    await page.waitForSelector(saveButtonSelector); // Wait for save button to appear
+    await page.click(veryInterestedSelector); // click very interested
+    await page.click(saveButtonSelector); // click save
+
+    // Wait for rank button to appear and update conditions
+    await page.waitForSelector(rankButtonSelector); 
+    hasRankButton = page.$(rankButtonSelector);
+    ranked += 1;
+  }
+
+  // Close browser after 20 seconds (if you want you can close it immediately by clicking 'x')
+  await page.evaluate(async () => {
+    console.log('Ranking automation completed.')
+    console.log('Closing browser in 20 seconds.')
+  })
+  setTimeout(async () => {
+    await page.evaluate(() => {alert('Close browser')})
+    await browser.close();
+  }, 20000)
 }
 
 
-console.log(username, password)
+rank();
